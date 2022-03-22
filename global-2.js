2cd38a085b430273530609775d8bc9d9fd7f7bb6
Eoin Carrick <22carri99@gmail.com>
2022-03-22 21:44:45 +0000

    first commit

---
+++ global-2.js
@@ -0,0 +1,11 @@
+//Global Variable in NodeJS
+
+// console.log(`This is the path: ${__dirname}`);
+// console.log(`This is the file name: ${__filename}`);
+// console.log(`This is the require keyword: ${require}`);
+// console.log(`This is the module keyword: ${module}`);
+// console.log(`This is the process keyword: ${process}`);
+
+setInterval(() => {
+  console.log('Eoin, Welcome to the node World! ');
+}, 1000);
