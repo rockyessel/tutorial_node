bfb3950f9bdcc981095e34ee91aeb6112baa55d3
Eoin Carrick <22carri99@gmail.com>
2022-03-23 21:02:00 +0000

    code

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ const { readFileSync, writeFileSync } = require('fs');
 const first = readFileSync('./content/first.txt', 'utf8');
 const second = readFileSync('./content/second.txt', 'utf8');
 
-console.log(first, second);
+// console.log(first, second);
+
+writeFileSync(
+  './content/result.txt',
+  `Here is the result for the first, and second input: ${first}, ${second}`
+);
