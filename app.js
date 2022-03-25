2b63215bfc8f91607f21ac3c8e9ab30a92163dde
Eoin Carrick <22carri99@gmail.com>
2022-03-25 20:25:52 +0000

    code

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-const { readFile} = require('fs');
+const { readFile, writeFile } = require('fs');
+const util = require('util');
 
-readFile('./content/first.txt', 'utf8', function (error, result))
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+const start = async (path) => {
+  try {
+    const first = await readFilePromise(path, 'utf8');
+    await writeFilePromise(
+      './content/third.txt',
+      'The is the mind of my mind, where my mind is minding it own bisis.'
+    );
+    console.log(first);
+  } catch (error) {
+    console.log(`the error message: ${error}`);
+  }
+};
+
+start('./content/first.txt');
+start('./content/second.txt');
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', function (error, result) {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve(result);
+//       }
+//     });
+//   });
+// };
+
+// getText('./content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
