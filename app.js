bcf501f566ed2a8736401e910cec6bdb068c9114
Eoin Carrick <22carri99@gmail.com>
2022-03-25 21:10:57 +0000

    cdeo

--- app.js
+++ app.js
@@ -1,37 +1,4 @@
-const { readFile, writeFile } = require('fs');
-const util = require('util');
+const EventEmitter = require('events');
 
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
-
-const start = async (path) => {
-  try {
-    const first = await readFilePromise(path, 'utf8');
-    await writeFilePromise(
-      './content/third.txt',
-      'The is the mind of my mind, where my mind is minding it own bisis.'
-    );
-    console.log(first);
-  } catch (error) {
-    console.log(`the error message: ${error}`);
-  }
-};
-
-start('./content/first.txt');
-start('./content/second.txt');
-
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, 'utf8', function (error, result) {
-//       if (error) {
-//         reject(error);
-//       } else {
-//         resolve(result);
-//       }
-//     });
-//   });
-// };
-
-// getText('./content/first.txt')
-//   .then((result) => console.log(result))
-//   .catch((error) => console.log(error));
+const customEmitter = new EventEmitter();
+console.log(customEmitter);
