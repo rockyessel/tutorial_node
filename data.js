18d46912c5ec0dd89849e603efe844d062bb87aa
Eoin Carrick <22carri99@gmail.com>
2022-03-29 15:50:14 +0000

    a[[

---
+++ data.js
@@ -0,0 +1,317 @@
+const products = [
+  {
+    id: 1,
+    username: 'uponpassive',
+    profileURL: 'https://images2.imgbox.com/0b/bc/osNC6GCT_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 2,
+    username: 'glaziermediocre',
+    profileURL: 'https://images2.imgbox.com/b7/b1/O6ur4UDY_o.j',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 3,
+    username: 'descentbackstay',
+    profileURL: 'https://images2.imgbox.com/93/45/GOJOQroq_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 4,
+    username: 'fletchingminer',
+    profileURL: 'https://images2.imgbox.com/21/20/Qn776ERJ_o.jpeg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 5,
+    username: 'blurtshyster',
+    profileURL: 'https://images2.imgbox.com/0b/bc/osNC6GCT_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 6,
+    username: 'trackouthaul',
+    profileURL: 'https://images2.imgbox.com/30/06/rhPzqKoF_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 7,
+    username: 'specificpair',
+    profileURL: 'https://images2.imgbox.com/0b/bc/osNC6GCT_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 8,
+    username: 'birthdaynativity',
+    profileURL: 'https://images2.imgbox.com/b3/cb/RS7EalSu_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 9,
+    username: 'poinsettiademocrat',
+    profileURL: 'https://images2.imgbox.com/0b/bc/osNC6GCT_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 10,
+    username: 'neverthorns',
+    profileURL: 'https://images2.imgbox.com/60/48/nmGOxy1U_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 11,
+    username: 'sillycycling',
+    profileURL: 'https://images2.imgbox.com/e4/79/F8vkmw4I_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 12,
+    username: 'crownslice',
+    profileURL: 'https://images2.imgbox.com/09/ef/ZqXGR7Fx_o.png',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 13,
+    username: 'yawningcomposed',
+    profileURL: 'https://images2.imgbox.com/0b/bc/osNC6GCT_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 14,
+    username: 'totallyactual',
+    profileURL: 'https://images2.imgbox.com/6e/bb/b53D6yIM_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 15,
+    username: 'oilybozo',
+    profileURL: 'https://images2.imgbox.com/78/e7/TJ2Ibrza_o.png',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 16,
+    username: 'cabinetjournal',
+    profileURL: 'https://images2.imgbox.com/34/ba/QM06LcnO_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 17,
+    username: 'rashnun',
+    profileURL: 'https://images2.imgbox.com/ed/b6/bCXfUGUv_o.png',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 18,
+    username: 'sensitiveexample',
+    profileURL: 'https://images2.imgbox.com/d5/31/3qp9QNtG_o.png',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 19,
+    username: 'animalearnings',
+    profileURL: 'https://images2.imgbox.com/b3/cb/RS7EalSu_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 20,
+    username: 'pigletpanel',
+    profileURL: 'https://images2.imgbox.com/e4/79/F8vkmw4I_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12K',
+  },
+  {
+    id: 21,
+    username: 'fatherlyaustere',
+    profileURL: 'https://images2.imgbox.com/0b/bc/osNC6GCT_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12',
+  },
+  {
+    id: 22,
+    username: 'unwrapgratified',
+    profileURL: 'https://images2.imgbox.com/7c/b5/9LH5SUow_o.png',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12',
+  },
+  {
+    id: 23,
+    username: 'skicandlelight',
+    profileURL: 'https://images2.imgbox.com/0b/bc/osNC6GCT_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12',
+  },
+  {
+    id: 24,
+    username: 'axiomaticslender',
+    profileURL: 'https://images2.imgbox.com/60/48/nmGOxy1U_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12',
+  },
+  {
+    id: 24,
+    username: 'eoincarrick',
+    profileURL: 'https://images2.imgbox.com/e4/79/F8vkmw4I_o.jpg',
+    codeSpace: {
+      GitHub: 'https://images2.imgbox.com/c8/6b/8G8QpT4D_o.png',
+      GitLab: 'https://images2.imgbox.com/a5/64/rIIRtsgL_o.png',
+      GitBucket: 'https://images2.imgbox.com/a2/c2/TxTyIT3e_o.png',
+      CodePen: 'https://images2.imgbox.com/5d/89/sGWApi2K_o.png',
+    },
+    Repositories: '12',
+  },
+];
+
+const people = products.map((name_id, index) => {
+  const { id, username } = name_id;
+  return { id, username };
+});
+
+module.exports = { products, people };
+
+// [
+//   { id: 1, name: `Carrick` },
+//   { id: 2, name: `John` },
+//   { id: 3, name: `Eoin` },
+//   { id: 4, name: `Michael` },
+//   { id: 5, name: `Sera` },
+// ];
