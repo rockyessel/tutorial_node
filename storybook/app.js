1df6a2c1ff0c1e5ef18b92e24d450d7c8b1ab12b
Eoin Carrick <22carri99@gmail.com>
2022-04-03 22:27:10 +0000

    add

--- storybook/app.js
+++ storybook/app.js
@@ -6,7 +6,7 @@ const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db');
 
 // Load config
@@ -41,9 +41,7 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({
-    mongooseConnection: mongoose.connection;
-    })
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
   })
 );
 
