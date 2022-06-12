681ff9921583326c580484c2685f108a19780b29
Eoin Carrick <22carri99@gmail.com>
2022-06-12 00:30:03 +0000

    save

---
+++ paystack/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+
+
+
+app.post('/api.')
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
